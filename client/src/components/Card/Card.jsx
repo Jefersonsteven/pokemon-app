546858fbb7642183cc406fbd82903d587ccbbf6d
Gfrey <gfreycode@gmail.com>
2023-04-22 02:06:07 -0500

    dault-image

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -9,19 +9,19 @@ import getColorPerType from '../../assets/getColorPerType';
 function Card({ id, image, name, types }) {
     const dispatch = useDispatch();
 
-    return ( 
+    return (
         <div className="CardPokemon">
-            {!image && <span>Loading</span> }
-            <img className="Image" src={image} alt={name} />
+            {!image && <span>Loading</span>}
+            <img className="Image" src={image} alt={name} onerror="this.src='../../assets/images/default-card.png';" />
             <div className="Info">
                 <h3 className="Name" onClick={() => dispatch(setPokemon())}>
                     <Link to={`/pokemon/${id}`}>{name}</Link>
                 </h3>
                 <div className="Types">
                     {
-                        types?.length > 0 && 
-                        types?.map((type, index) => ( 
-                            <span key={index + "UUID"} style={{backgroundColor: getColorPerType(type)}}>{type}</span> 
+                        types?.length > 0 &&
+                        types?.map((type, index) => (
+                            <span key={index + "UUID"} style={{ backgroundColor: getColorPerType(type) }}>{type}</span>
                         ))
                     }
                 </div>
